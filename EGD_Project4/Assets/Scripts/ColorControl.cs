6f53b920e8bd403d7d0144dacacab0c0384e7b78
CocoTang03 <qinwentang@outlook.com>
2023-10-02 18:00:29 -0400

    Change Language to Eng; Add a few stuff in c# scripts.

--- EGD_Project4/Assets/Scripts/ColorControl.cs
+++ EGD_Project4/Assets/Scripts/ColorControl.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 using Yarn.Unity;
 public class ColorControl : MonoBehaviour
 {
+    private DialogueRunner runner;
+    private static int days;
+    private static float mood;
+    //private const 
+
+    public void Start()
+    {
+        runner = GameObject.FindObjectOfType<DialogueRunner>();
+        runner.AddFunction<int>("get_day_num", GetDayNum);
+    }
+
+    private static int GetDayNum()
+    {
+        return days;
+    }
 
     [YarnCommand("whatever")]
     public void testPrint()

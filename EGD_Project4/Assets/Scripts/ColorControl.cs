20ca2f2abc2e9d2c5396c8426c8e8f003d872cc9
CocoTang03 <qinwentang@outlook.com>
2023-10-03 04:40:27 -0400

    Add some sample functions that might be helpful

--- EGD_Project4/Assets/Scripts/ColorControl.cs
+++ EGD_Project4/Assets/Scripts/ColorControl.cs
@@ -7,22 +7,46 @@ public class ColorControl : MonoBehaviour
     private DialogueRunner runner;
     private static int days;
     private static float mood;
-    //private const 
 
-    public void Start()
+    public AudioSource click;
+    //private const 
+    public void Awake()
     {
         runner = GameObject.FindObjectOfType<DialogueRunner>();
-        runner.AddFunction<int>("get_day_num", GetDayNum);
+        runner.AddFunction<int>("day_num", GetDayNum);
+        runner.AddCommandHandler<float>("change_mood", ChangeMood);
+        runner.AddCommandHandler<string, string>("setting", PlaceCharacter);
+    }
+    public void Start()
+    {
     }
 
     private static int GetDayNum()
     {
         return days;
     }
-
-    [YarnCommand("whatever")]
-    public void testPrint()
+    private static void ChangeMood(float value)
     {
-        Debug.Log("This function get used");
+        mood += value;
+    }
+    private static void SwitchCharacter()
+    {
+
+    }
+
+    private static void SwitchBackground()
+    {
+
+    }
+
+    // Put character asset into the right spot on the screen.
+    private void PlaceCharacter(string characterName, string markerName)
+    {
+
+    }
+
+    private void ChoiceSelected()
+    {
+        click.Play();
     }
 }

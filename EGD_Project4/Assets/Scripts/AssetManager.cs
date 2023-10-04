831d522cc0b0ac18fcd221c2c99a9dde979bfdcc
CocoTang03 <qinwentang@outlook.com>
2023-10-04 00:08:04 -0400

    Upload art assets and layout

---
+++ EGD_Project4/Assets/Scripts/AssetManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AssetManager : MonoBehaviour
+{
+    public Image background;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
